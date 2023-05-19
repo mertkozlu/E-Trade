2f2790699bb3367caa622a4d378d8e67a7cdd11a
mertkozlu <kozlumert@outlook.com>
2023-05-19 10:43:55 +0300

    k

--- src/main/java/com/ETrade/Application.java
+++ src/main/java/com/ETrade/Application.java
@@ -1,9 +1,19 @@
 package com.ETrade;
 
+import com.ETrade.core.utilities.exceptions.BusinessException;
+import com.ETrade.core.utilities.exceptions.ProblemDetails;
+import com.ETrade.core.utilities.exceptions.ValidationProblemDetails;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.HashMap;
 
 @SpringBootApplication
 public class Application {
@@ -12,9 +22,32 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@ExceptionHandler
+	@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+	public ProblemDetails handleBusinessException(BusinessException businessException) {
+		ProblemDetails problemDetails = new ProblemDetails();
+		problemDetails.setMessage(businessException.getMessage());
+		return problemDetails;
+	}
+
+	@ExceptionHandler
+	@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+	public ProblemDetails handleBusinessException(MethodArgumentNotValidException methodArgumentNotValidException) {
+		ValidationProblemDetails validationProblemDetails = new ValidationProblemDetails();
+		validationProblemDetails.setMessage("VALİDATİON:EXCEPTİON");
+		validationProblemDetails.setValidationErrors(new HashMap<String, String>());
+
+		for (FieldError fieldError : methodArgumentNotValidException.getBindingResult().getFieldErrors()) {
+			validationProblemDetails.getValidationErrors().put(fieldError.getField(), fieldError.getDefaultMessage());
+		}
+		return validationProblemDetails;
+	}
+
 	@Bean
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
 	}
+
+
 
 }
